6c59ce7dc2eadadc2f68e3146375ed3c5d5d4a1c
Shahzain Zaidi <143355259+theshahzayn@users.noreply.github.com>
2024-05-07 19:21:40 -0400

    .bin8.2.c

--- .bin8.2.c
+++ .bin8.2.c
@@ -29,3 +29,53 @@ int main(int argc, char* argv[]){
 	return 0;
 
 }
+
+
+/*
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t m;
+
+void *table(void *num){
+    int n = *((int *)num);
+
+    pthread_mutex_lock(&m);
+
+    for(int i=1; i<=1000; i++){
+        printf("%d x %d = %d\n", n, i, i * n);
+    }
+
+    pthread_mutex_unlock(&m);
+
+    return NULL; 
+}
+
+int main(){
+    pthread_mutex_init(&m, NULL);
+
+    pthread_t t[4];
+    int rc;
+
+    for(int i=0; i<4; i++){
+        int num = 5 + i;
+        rc = pthread_create(&t[i], NULL, table, &num); 
+        if(rc != 0){
+            printf("Error: Thread not created\n");
+        }
+    }
+
+    for(int i = 0; i < 4; i++){
+        pthread_join(t[i], NULL);
+        if(rc != 0) {
+            printf("Error: Thread not exited\n");
+        }
+    }
+    pthread_mutex_destroy(&m);
+
+    return 0;
+}
+
+*/
